b42c9e009e4e10c7c8df29511207f75ac476f164
dash1ne1dash <72750663+dash1ne1dash@users.noreply.github.com>
2020-10-25 21:55:43 +0400

    cинхронизируйся еб твою мать

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,20 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 	int m=0;
 	System.out.print(m);
 
 	int k=m;
         System.out.print(k);
         System.out.print("фон бет самые быстрые выигрыши");
+        m=sc.nextInt();
         int f=m+k;
         System.out.println(f);
+        System.out.println("я синхронизировался я синхронизировался я синхронизировался я синхронизировался");
     }
 }
